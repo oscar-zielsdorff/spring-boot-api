bfe0fa2872b2264fb7950f5439aeacee8c1bd053
Oscar Zielsdorff <oscar.zielsdorff@gmail.com>
2024-04-03 00:03:05 -0700

    add pagination to a resource

--- src/main/java/com/example/oz/springbootapi/repository/TourRepository.java
+++ src/main/java/com/example/oz/springbootapi/repository/TourRepository.java
@@ -1,13 +1,15 @@
 package com.example.oz.springbootapi.repository;
 
 import com.example.oz.springbootapi.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends JpaRepository<Tour, Integer> {
 
     // Some older versions of spring require this @Param annotation
-    List<Tour> findByTourPackageCode(@Param("code")String code);
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
 }
